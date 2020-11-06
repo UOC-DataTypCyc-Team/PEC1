1179bf19d2ce81d8cd55583cb4d89f6d7bd0b1ba
Aitor Jara <aitorjara99@gmail.com>
2020-11-06 14:20:00 +0100

    refactor: Extract products from the list of links

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from webscraping import WebScrapper
 
 #Parameters
-URL = 'https://www.tuaceitedemotor.com/aceite-5w30-long-life-longlife-c102x2453154'
+#URL = 'https://www.tuaceitedemotor.com/aceite-5w30-long-life-longlife-c102x2453154'
+URL2 = 'https://www.tuaceitedemotor.com/'
+NUM_LINKS = 20
 
 if __name__ == "__main__":
-    webscrapper = WebScrapper(url=URL)
+    webscrapper = WebScrapper(url=URL2, num_links=NUM_LINKS)
     webscrapper.print_dataframe()
-    webscrapper.export_csv()
+    #webscrapper.export_csv()
--- webscraping.py
+++ webscraping.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re
 
 
 class WebScrapper():
-    def __init__(self, url):
+    def __init__(self, url, num_links):
         """
         The constructor for the WebScrapper class.
 
@@ -23,6 +23,7 @@ class WebScrapper():
         self.oil_quantity_list=[]
         self.oil_price_list = []
         self.links= []
+        self.num_links = num_links
 
 
     def get_request(self, url=None):
@@ -34,8 +35,10 @@ class WebScrapper():
         """
         url = self.url if url is None else url
         req = requests.get(url)
-        page = soup(req.text, 'html.parser')
-        return page
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        return soup
+
 
     # Coge todos los links de una web y los añade a la lista "links" del objeto (self.links)
     def get_links(self, url=None):
@@ -44,33 +47,37 @@ class WebScrapper():
 
         for tag in tags:
             try:
-                semilink = re.findall('^[^h\s].*',tag.get('href', None))
-                link = re.findall('^http.*',tag.get('href', None))
-    
-                semilink = 'https://www.tuaceitedemotor.com/' + semilink[0]
-                if semilink not in self.links:
-                    self.links.append(semilink)
-                    print(semilink)
-                elif link:
-                    link = link[0]
-                    if link not in self.links:
-                        self.links.append(link)
-                        print(link)
+                if len(self.links) <= self.num_links:  
+                    semilink = re.findall('^[^h\s].*',tag.get('href', None))
+                    semilink = self.url + semilink[0]
+                    link = re.findall('^http.*',tag.get('href', None))
+        
+                    if semilink and semilink not in self.links:
+                        self.links.append(semilink)
+                        #print(semilink)
+                    elif link:
+                        link = link[0]
+                        if link not in self.links:
+                            self.links.append(link)
+                            #print(link)
+                else:
+                    break
+                            
             except: 
                 continue
 
-        print(len(self.links))
 
-    # Limitamos la red a unos cuantos enlaces llamando a "get_links" hasta llegar a ese tamaño
+    # Limitamos el tamaño de la red a unos cuantos enlaces llamando a "get_links" hasta llegar al tamaño que queremos
     def net(self):
 
         self.get_links()
         for link in self.links:
-            if len(self.links) <= 150:  
+            if len(self.links) <= self.num_links:  
                 self.get_links(link)
             else:
-                print("Spyder done")
+                print('\nThe webcrawler has retrieved {} web-pages successfully!!!'.format(len(self.links)))
                 break
+
 
     def parse(self):
         """
@@ -81,32 +88,35 @@ class WebScrapper():
             oil_price_list (lst): A list containing the prices of the different oil products.
         """
 
-        # Function that calls the function get_request() to obtain the web information and is saved in the variable page
-        page = self.get_request()
+        # Llamamos a self.net para parsear el primer enlace
+        self.net()
 
+        # Del listado de enlaces que hemos conseguido, llamamos a get_request para parsear todos ellos y crear la base de datos
+        for link in self.links:
+            try:            
+                soup = self.get_request(link)
+                oil_list = soup.find_all('div', {'class': 'sectiondataarea'})
 
-        oil_list = page.find_all('div', {'class': 'sectiondataarea'})
+            except:
+                continue
+            
+            for i in range(1, len(oil_list)):
+                try:
+                    oil_name = oil_list[i].find('div', {'class': 'PBItemName'}).text.rstrip()
+                    oil_brand = oil_name.split(' ')[0]
+                    oil_quantity = oil_name.split(' ')[-1].replace('L','')
+                    oil_price = oil_list[i].find('span', {'class': 'PBSalesPrice'}).text
+                    oil_price = float(oil_price.replace(' EUR','').replace(',','.'))
 
+                    self.oil_name_list.append(oil_name)
+                    self.oil_brand_list.append(oil_brand)
+                    self.oil_quantity_list.append(oil_quantity)
+                    self.oil_price_list.append(oil_price)
 
+                except:
+                    continue
 
-        for i in range(1, len(oil_list)):
-
-            oil_name = oil_list[i].find('div', {'class': 'PBItemName'}).text.rstrip()
-
-            oil_brand = oil_name.split(' ')[0]
-
-            oil_quantity = oil_name.split(' ')[-1].replace('L','')
-
-            oil_price = oil_list[i].find('span', {'class': 'PBSalesPrice'}).text
-            oil_price = float(oil_price.replace(' EUR','').replace(',','.'))
-
-            self.oil_name_list.append(oil_name)
-
-            self.oil_brand_list.append(oil_brand)
-
-            self.oil_quantity_list.append(oil_quantity)
-
-            self.oil_price_list.append(oil_price)
+        print('\n{} products have been parsed successfully!!!'.format(len(self.oil_name_list)))
 
         return self.oil_name_list, self.oil_brand_list, self.oil_quantity_list, self.oil_price_list
 
@@ -122,6 +132,7 @@ class WebScrapper():
         oil_name_list, oil_brand_list, oil_quantity_list, oil_price_list = self.parse()
         data_tuples = list(zip(oil_name_list, oil_brand_list, oil_quantity_list, oil_price_list))
         df = pd.DataFrame(data_tuples, columns=['Name','Brand', 'Quantity Liters', 'Price'])
+
         return df
 
 
@@ -134,8 +145,9 @@ class WebScrapper():
         """
 
         df = self.lists_to_dataframe()
-        print(df)
+        print('\n\n',df.head(100))
         #df.head()
+
 
     def export_csv(self) -> object:
         """
@@ -147,12 +159,16 @@ class WebScrapper():
 
         self.lists_to_dataframe().to_csv('data.csv', index = False)
 
+
 ######################################   RUN   ######################################
 
 
 if __name__ == "__main__":
-    URL = 'https://www.tuaceitedemotor.com/aceite-5w30-long-life-longlife-c102x2453154'
+    #URL = 'https://www.tuaceitedemotor.com/aceite-5w30-long-life-longlife-c102x2453154'
     URL2 = 'https://www.tuaceitedemotor.com/'
-    webscrapper = WebScrapper(url=URL2)
-    webscrapper.net()
+    NUM_LINKS = 20
+
+    webscrapper = WebScrapper(url=URL2, num_links=NUM_LINKS)
+    webscrapper.print_dataframe()
+
 
